f114132d0be484bfac88a8cf96d0f8297052c37d
Sneha Kalra <snehakalra74@gmail.com>
2026-05-17 22:50:55 +0530

    Project Setup

---
+++ src/test/java/com/bakeaura/bakeaura_backend/BakeauraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakeaura.bakeaura_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeauraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
